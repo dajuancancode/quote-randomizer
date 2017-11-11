fd526f4d8fc8ba89dc93560c81da255d018fcf23
DaJuan Harris <dajuancancode@gmail.com>
2017-11-10 21:15:34 -0500

    Fixed JavaScript so that the same quote would not be repeated twice

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,26 @@ const mainBackground = document.querySelector('main');
 let red;
 let blue;
 let green;
-let viewedList
+let viewedQuotes
 
-viewedList = []
+viewedQuotes = []
+
+let randomNum = () => Math.floor(Math.random() * (quotes.length))
+
 newQuote.addEventListener('click', () => {
-    let randomNum = () => Math.floor(Math.random() * (quotes.length))
+
+    if(quotes.length === 0){
+        quotes = viewedQuotes;
+        viewedQuotes = [];
+    }
+    
+    
 
     i = randomNum()
 
     quoteContainer.textContent = quotes[i].quote;
     authorContainer.textContent = '- ' + quotes[i].author;
+    
+    let splicedQuote = quotes.splice(i, 1)[0]
+    viewedQuotes.push(splicedQuote)
 })
